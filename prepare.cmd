e229672a645bd5e01e1b7a8db479e0b14e860fd4
wagner <wagner@rg.mpg.de>
2020-09-21 14:03:19 +0200

    Improve documentation for preparatory routines.

--- prepare.cmd
+++ prepare.cmd
@@ -3,7 +3,13 @@
 :: Author:   wagner@rg.mpg.de
 :: Revision: Sept 2020
 
-:: see https://stackoverflow.com/a/57449284/4042645
+:: Xmllint/xmltproc seem to be among the VERY few tools able to resolve our XIncludes (since they make use of
+:: the xpointer scheme in the xpointer attribute) besides eXist-db and our online web application.
+:: See https://www.zlatkovic.com/projects/libxml/index.html for download links and more information.
+::
+:: For more conventional XSLT processing, we're using Saxon Home Edition.
+::
+:: We include all the necessary files here for convenience.
 
 @echo off
 set JAVA_BIN="C:\Program Files (x86)\Common Files\Oracle\Java\javapath\java.exe"
@@ -12,5 +18,4 @@ set JAVA_BIN="C:\Program Files (x86)\Common Files\Oracle\Java\javapath\java.exe"
 .\prepare\xmllint.exe --nowarning --xinclude --nsclean "temp1.xml" > "temp2.xml"
 %JAVA_BIN% -cp "./prepare/saxon9he.jar" net.sf.saxon.Transform -s:"temp2.xml" -xsl:"./prepare/2_use_zenodo_vocab.xsl" -o:%2
 
-:: --postvalid 
 del "temp1.xml" "temp2.xml"
